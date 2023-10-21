dfaa48da6e04554e93ba93e301beba87ca7db2e6
khairul-islam98 <janeyrahman900@gmail.com>
2023-10-21 19:54:49 +0600

    added effect in home page

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
         <div className=''>
             <Banner></Banner>
             <h2 className='text-5xl  text-blue-500 dark:text-white font-rancho text-center mt-14 '>Our Trusted  Brands</h2>
-            <div className='grid  md:grid-cols-2 lg:grid-cols-3 gap-6  py-5 ml-6  mr-6'>
+            <div className='grid  md:grid-cols-2 lg:grid-cols-3 gap-6  py-5 ml-6  mr-6 '>
                 {brands.map(brand =>
                     <Link key={brand._id} to={`/brand/${brand.brandName}`}>
-                        <div className="relative dark:bg-gray-800 shadow-slate-200 flex flex-col rounded-xl h-[80%] bg-white bg-clip-border text-gray-700 shadow-md">
+                        <div className="relative dark:bg-gray-800 shadow-slate-200 flex flex-col rounded-xl h-[80%] bg-white bg-clip-border text-gray-700 shadow-md transition ease-in-out  delay-150 hover:-translate-y-1 hover:scale-110  duration-300">
                             <div className="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
                                 <img className='h-full w-full' src={brand.photo} alt="brand photo" />
                             </div>
--- src/components/BrandProduct/BrandProduct.jsx
+++ src/components/BrandProduct/BrandProduct.jsx
@@ -32,12 +32,12 @@ const BrandProduct = () => {
                                     {product?.name}
                                 </h5>
                                 <div>
-                                <h5 className="block text-xl antialiased font-semibold leading-snug tracking-normal text-red-500">
-                                    {product?.brandName}
-                                </h5>
-                                <h5 className="block mb-2  text-xl antialiased font-semibold leading-snug tracking-normal text-blue-500">
-                                    {product?.type}
-                                </h5>
+                                    <h5 className="block text-xl antialiased font-semibold leading-snug tracking-normal text-red-500">
+                                        {product?.brandName}
+                                    </h5>
+                                    <h5 className="block mb-2  text-xl antialiased font-semibold leading-snug tracking-normal text-blue-500">
+                                        {product?.type}
+                                    </h5>
                                 </div>
                             </div>
                             <p className="block font-ralway text-base antialiased font-light leading-relaxed text-inherit">
@@ -46,20 +46,20 @@ const BrandProduct = () => {
                             <div className='flex justify-between mt-2'>
                                 <h2 className='text-lg font-rancho'>Price: {product?.price} $</h2>
                                 <div className='flex justify-center items-center gap-1'>
-                                    <p className='text-lg text-[#dbc085]'><AiTwotoneStar></AiTwotoneStar></p>
-                                    <h5>{product.rating}</h5>
+                                    <p className='text-2xl text-[#dbc085]'><AiTwotoneStar></AiTwotoneStar></p>
+                                    <h5 className='text-xl'>{product?.rating}</h5>
                                 </div>
                             </div>
                         </div>
                         <div className="flex-col text-center lg:flex">
                             <Link to={`/productdetails/${product._id}`}>
-                            <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  font-medium rounded-lg text-sm px-20 py-2.5 w-full   mb-2'>Details</button>
+                                <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  font-medium rounded-lg text-sm px-20 py-2.5 w-full   mb-2'>Details</button>
                             </Link>
                             <Link to={`/productupdate/${product._id}`}>
-                            <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm  py-2.5 px-20 w-full  mb-2 '>Update</button>
+                                <button className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm  py-2.5 px-20 w-full  mb-2 '>Update</button>
                             </Link>
                         </div>
-                        
+
                     </div>)
                 }
             </div>
